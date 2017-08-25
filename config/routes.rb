7ee1ab87b4de3bdcf9728c70df2893f0f36a69b4
Md Azharuddin <mohammad.azhar2000@gmail.com>
2017-08-26 00:39:11 +0530

    Adding CSS

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   get 'emicalc/calc'
-  post 'emicalc/calc'
+  post "/" => "emicalc#calc"
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
